f8ed6452cb737060341a4db2c681a6fde2e2fd86
Ivan Kirshak <i.kirshak@gmail.com>
2020-01-14 16:53:33 +0200

    added contact form and minor ui update

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
+// DOM VARIABLES 
+let form = document.getElementById("form");
+let business = document.getElementById("selectBusiness");
+let waste = document.getElementById("selectWasteType");
+let address = document.getElementById("address");
+let date = document.getElementById("visitDate");
+let button = document.getElementById("btnSubmit");
+let answer = document.getElementById("answer");
+
 // TOGGLE MOBILE HEADER
 
 let header = document.getElementById('invisible-header');
+let btn = document.getElementById('bttn-mobile');
 
 function trial() {
     if (header.style.display === 'none') {
@@ -10,13 +20,14 @@ function trial() {
         header.style.textAlign = 'center';
         header.style.padding = '0.5rem';
         header.style.fontFamily = 'reross-rectangular';
-        header.style.transition = 'all 0.3s';
-        document.getElementById('bttn-mobile').innerHTML = 'close menu';
+        btn.innerHTML = 'close menu';
     } else {
         header.style.display = 'none';
-        document.getElementById('bttn-mobile').innerHTML = 'Menu';
+        btn.innerHTML = 'Menu';
     }
 }
+
+btn.addEventListener("click", trial, false);
 
 // SMOOTH SCROLLING FROM HEADER TO TOPICS
 
@@ -35,3 +46,20 @@ document.querySelector('#foot').scrollIntoView({
 document.querySelector('#products').scrollIntoView({
     behavior: 'smooth'
 });
+
+// FORM HANDLING 
+function formHandle() {
+    let businessVal = business.value;
+    let wasteVal = waste.value;
+    let addressVal = address.value;
+    let dateVal = new Date(date.value).toLocaleDateString();
+    if (addressVal != "" && dateVal != "") {
+        answer.innerHTML = `<h1>The order has been sent. We are creating ${businessVal} solution for You. We will take away your ${wasteVal} waste from ${addressVal} on ${dateVal}.</h1>`;
+        form.classList.remove("alert");
+    } else {
+        answer.innerHTML = `<h1 style="color: red">Please, fill the form</h1>`;
+        form.classList.add("alert");
+    }
+}
+
+button.addEventListener("click", formHandle, false);
